96102b8ad958124a560a7ab82be122a011df3506
LohitaaRajan <lohitaa.prabhurajan@jpschools.org>
2026-06-09 16:03:13 +0000

    Done for today

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,7 +27,7 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
+    // toggleGrid();
 
     // TODO 2 - Create Platforms
     createPlatform(250, 625, 150, 50, "midnightblue");
@@ -38,12 +38,20 @@ $(function () {
     createPlatform(875, 315, 150, 50, "midnightblue");
     createPlatform(100, 200, 700, 50, "midnightblue");
     createPlatform(175, 100, 50, 15, "midnightblue");
-    createPlatform(1110, 625, 150, 50, "midnightblue");
+    createPlatform(1100, 625, 150, 50, "midnightblue");
+    createBadPlatform(1100, 425, 150, 50, "red");
 
     // TODO 3 - Create Collectables
     createCollectable("database", 180, 1, 0.2);
+    createCollectable("database", 1155, 525, 0.2);
+    createCollectable("database", 125, 105, 0.2);
+    createCollectable("database", 930, 425, 0.2);
+    createCollectable("database", 500, 400, 0.2);
 
     // TODO 4 - Create Cannons
+    createCannon("right", 280, 2000);
+    createCannon("right", 150, 1500);
+    createCannon("right", 700, 1500);
 
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
